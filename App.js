fb5fdaf04845c62d84126d7d4a7c33d6d57440cc
Ooey <agiospantas@gmail.com>
2023-01-14 01:42:42 +0200

    custom calendar component and some basic css

--- App.js
+++ App.js
@@ -1,20 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import { StyleSheet, Text, View } from 'react-native'
+import { Calendar } from './Components/Calendar'
+import { FloatingButton } from './Components/FloatingButton'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar style="light" translucent={false} />
+      <Calendar />
+      <FloatingButton />
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'rgba(50,50,50,0.9)',
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+  header: {
+    height: 100,
+    width: '100%',
+    backgroundColor: 'red',
+  },
+})
